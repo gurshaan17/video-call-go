686df3010d97ba34c3996206ac54f214b69d91e2
gurshaan17 <gurshaansinghd@gmail.com>
2024-11-11 22:49:01 +0530

    basic structure set up

--- internal/server/server.go
+++ internal/server/server.go
@@ -12,7 +12,21 @@ var (
 )
 
 
+func Run () error {
+	flag.Parse()
+	if *addr == ":" {
+		*addr = ":8080"
+	}
 
-func server(){
-
+	app.Get("/", handlers.Welcome)
+	app.Get("/room/create", handlers.RoomCreate)
+	app.Get("/room/:uuid", handlers.Room)
+	app.Get("/room/:uuid/websocket", )
+	app.Get("/room/:uuid/chat", handlers.RoomChat)
+	app.Get("/room/:uuid/chat/websocket", websocket.New(handlers.RoomChatWebsocket))
+	app.Get("/room/:uuid/viewer/websocket", websocket.New(handlers.RoomViewerWebsocket))
+	app.Get("/stream/:ssuid", handlers.Stream)
+	app.Get("/stream/:ssuid/websocket", )
+	app.Get("/stream/ssuid/chat/websocket", )
+	app.Get("/stream/:ssuid/viewer/websocket", )
 }
